952ef9e34d47d8c654f33f944f17cb449ba2439c
aashutoshPanda <iamashutoshpanda@gmail.com>
2021-03-07 15:40:50 +0530

    file upload & patch done

--- file/serializers.py
+++ file/serializers.py
@@ -1,16 +1,9 @@
 from rest_framework import serializers
 from .models import File
-import secrets
 
 
 class FileSerializer(serializers.ModelSerializer):
-    filesystem_id = serializers.SerializerMethodField()
 
     class Meta():
         model = File
         fields = ('file', 'filesystem_id', 'timestamp')
-
-    def get_filesystem_id(self, obj):
-        if "filesystem_id" in self.context:
-            return self.context["filesystem_id"]
-        return "nai tha"
--- file/views.py
+++ file/views.py
@@ -3,7 +3,7 @@ from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import FileSerializer
-
+import secrets
 import re
 from user.models import Profile
 from .models import File
@@ -40,29 +40,23 @@ class FileView(APIView):
             if(name == already_present_name and "FILE" == already_present_type):
                 return Response(data={"message": "Such a file/folder is already present"}, status=status.HTTP_400_BAD_REQUEST)
 
-        file_serializer = FileSerializer(
-            data=request.data, context={"filesystem_id": "123456"})
-
-        if file_serializer.is_valid():
-            file_serializer.save()
-            print("file serializer data", file_serializer.data)
-            # id = file_serializer.data["filesystem_id"]
-            # children[id] = {
-            #     "TYPE": "FILE",
-            #     "NAME": name
-            # }
-            # filesystem[parent]["CHILDREN"] = children
-            # filesystem[id] = {
-            #     "PARENT": parent,
-            #     "TYPE": "FILE",
-            #     "NAME": name
-            # }
-            # profile.filesystem = filesystem
-            # profile.save()
-
-            return Response(file_serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        filesystem_id = secrets.token_urlsafe(16)
+        file_obj = File(
+            file=request.FILES['file'], filesystem_id=filesystem_id)
+        file_obj.save()
+        children[filesystem_id] = {
+            "TYPE": "FILE",
+            "NAME": name
+        }
+        filesystem[parent]["CHILDREN"] = children
+        filesystem[filesystem_id] = {
+            "PARENT": parent,
+            "TYPE": "FILE",
+            "NAME": name
+        }
+        profile.filesystem = filesystem
+        profile.save()
+        return Response(data=filesystem, status=status.HTTP_200_OK)
 
     def patch(self, request, *args, **kwargs):
         profile = Profile.objects.get(user=request.user)
@@ -75,9 +69,9 @@ class FileView(APIView):
         new_name = request.data["new_name"]
 
         if file_id not in filesystem:
-            return Response(data={"message": "Invalid parent"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(data={"message": "Invalid file id"}, status=status.HTTP_400_BAD_REQUEST)
         elif re.match(REGEX_NAME, new_name) is None:
-            return Response(data={"message": "Invalid Name"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(data={"message": "Invalid New Name"}, status=status.HTTP_400_BAD_REQUEST)
 
         parent = filesystem[file_id]["PARENT"]
         children = filesystem[parent]["CHILDREN"]
@@ -87,25 +81,19 @@ class FileView(APIView):
             if(new_name == already_present_name and "FILE" == already_present_type):
                 return Response(data={"message": "Such a file/folder is already present"}, status=status.HTTP_400_BAD_REQUEST)
 
-        print(file_id)
         file_obj = File.objects.get(filesystem_id=file_id)
-        print(file_obj.file.name)
-        return Response("ok ok", status=status.HTTP_200_OK)
-        # if file_serializer.is_valid():
-        #     file_serializer.save()
-        #     id = file_serializer.data["filesystem_id"]
-        #     children[id] = {
-        #         "TYPE": "FILE",
-        #         "NAME": name
-        #     }
-        #     filesystem[parent]["CHILDREN"] = children
-        #     filesystem[id] = {
-        #         "PARENT": parent,
-        #         "TYPE": "FILE",
-        #         "NAME": name
-        #     }
-        #     profile.filesystem = filesystem
-        #     profile.save()
-        #     return Response(file_serializer.data, status=status.HTTP_201_CREATED)
-        # else:
-        #     return Response(file_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        file_obj.file.name = new_name
+        file_obj.save()
+        children[file_id] = {
+            "TYPE": "FILE",
+            "NAME": new_name
+        }
+        filesystem[parent]["CHILDREN"] = children
+        filesystem[file_id] = {
+            "PARENT": parent,
+            "TYPE": "FILE",
+            "NAME": new_name
+        }
+        profile.filesystem = filesystem
+        profile.save()
+        return Response(data=filesystem, status=status.HTTP_200_OK)
