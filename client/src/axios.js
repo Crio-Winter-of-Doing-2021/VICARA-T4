bcfc4a4f137079450f0df62f4956e71761c18702
Pratik <pchy.393@gmail.com>
2021-03-09 18:35:33 +0530

    changed token

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export let baseURL = "http://localhost:8000";
 
-let token = "Token b8955c3a2e945bb8e22aa000d6a7ef3dc3d66d07";
+let token = "Token 27aba61b494aa6ab4cf98097807baf110fff47f6";
 
 export default axios.create({
   baseURL,
