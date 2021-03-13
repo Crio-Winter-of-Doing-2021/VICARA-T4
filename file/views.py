5283357da547b2faf768fd7ad0137ab6920e512c
aashutoshPanda <iamashutoshpanda@gmail.com>
2021-03-13 15:01:49 +0530

    refactored /file

--- file/views.py
+++ file/views.py
@@ -1,3 +1,4 @@
+from varname import nameof
 from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework.response import Response
@@ -10,19 +11,25 @@ from .models import File
 from mysite.constants import *
 from mysite.decorators import *
 REQUIRED_POST_PARAMS = [PARENT, "file"]
-REQUIRED_PATCH_PARAMS = ["file_id", "new_name"]
-REQUIRED_DELETE_PARAMS = ["file_id"]
+REQUIRED_PATCH_PARAMS = ["id", "name"]
+REQUIRED_DELETE_PARAMS = ["id"]
 REGEX_NAME = r"^[\w\-. ]+$"
+
+
+def update_profile(profile, *args):
+    for attr in args:
+        setattr(profile, nameof(attr), attr)
+    profile.save()
 
 
 class FileView(APIView):
     parser_classes = (MultiPartParser, FormParser, JSONParser)
 
     @check_request_attr(REQUIRED_PARAMS=REQUIRED_POST_PARAMS)
-    @check_regex
     @check_parent
-    @check_type(type_required=FOLDER)
-    @check_already_present
+    @check_parent_is_folder
+    @check_file_name_from_request_file
+    @check_already_present(to_check="req_file_name", type=FILE)
     def post(self, request, *args, **kwargs):
 
         profile = Profile.objects.get(user=request.user)
@@ -30,7 +37,6 @@ class FileView(APIView):
         parent = request.data[PARENT]
         name = request.FILES['file'].name
         children = filesystem[parent][CHILDREN]
-
         filesystem_id = secrets.token_urlsafe(16)
         file_obj = File(
             file=request.FILES['file'], filesystem_id=filesystem_id)
@@ -47,56 +53,46 @@ class FileView(APIView):
             NAME: name,
             FAVOURITE: False
         }
-        profile.filesystem = filesystem
-        profile.save()
+        update_profile(profile, filesystem)
         return Response(data=filesystem, status=status.HTTP_200_OK)
 
     @check_request_attr(REQUIRED_PARAMS=REQUIRED_DELETE_PARAMS)
     @check_id
-    @check_type(type_required=FILE)
+    @check_type_id(type_required=FILE)
     def delete(self, request, *args, **kwargs):
         profile = Profile.objects.get(user=request.user)
         filesystem = profile.filesystem
         favourites = profile.favourites
         recent = profile.recent
-
-        file_id = request.data["file_id"]
-
-        parent = filesystem[file_id][PARENT]
-
-        filesystem[parent][CHILDREN].pop(file_id)
-        filesystem.pop(file_id)
-        if file_id in favourites:
-            favourites.pop(file_id)
+        id = request.data["id"]
+        parent = filesystem[id][PARENT]
+        filesystem[parent][CHILDREN].pop(id)
+        filesystem.pop(id)
+        if id in favourites:
+            favourites.pop(id)
         if id in recent:
-            recent.pop(file_id)
-
-        profile.filesystem = filesystem
-        profile.favourites = favourites
-        profile.recent = recent
-        profile.save()
+            recent.pop(id)
+        update_profile(profile, filesystem, favourites, recent)
         return Response(data=filesystem, status=status.HTTP_200_OK)
 
     @check_request_attr(REQUIRED_PARAMS=REQUIRED_PATCH_PARAMS)
     @check_id
-    @check_regex
-    @check_already_present
+    @check_regex_file_name_from_request_body
+    @check_equality_old_new_name
+    @check_already_present(to_check="req_data_name", type=FILE)
     def patch(self, request, *args, **kwargs):
+
+        id = request.data["id"]
+        new_name = request.data["name"]
         profile = Profile.objects.get(user=request.user)
         filesystem = profile.filesystem
-
-        file_id = request.data["file_id"]
-        new_name = request.data["new_name"]
-
-        parent = filesystem[file_id][PARENT]
+        parent = filesystem[id][PARENT]
         children = filesystem[parent][CHILDREN]
-
-        file_obj = File.objects.get(filesystem_id=file_id)
+        file_obj = File.objects.get(filesystem_id=id)
         file_obj.file.name = new_name
         file_obj.save()
-        children[file_id][NAME] = new_name
+        children[id][NAME] = new_name
         filesystem[parent][CHILDREN] = children
-        filesystem[file_id][NAME] = new_name
-        profile.filesystem = filesystem
-        profile.save()
+        filesystem[id][NAME] = new_name
+        update_profile(profile, filesystem, children)
         return Response(data=filesystem, status=status.HTTP_200_OK)
--- mysite/decorators.py
+++ mysite/decorators.py
@@ -1,5 +1,5 @@
 import functools
-from .constants import PARENT
+from .constants import FOLDER, PARENT
 from user.models import Profile
 from rest_framework.response import Response
 from rest_framework import status
@@ -23,28 +23,16 @@ def check_parent(func):
     return wrapper
 
 
-def check_regex(func):
-
-    @functools.wraps(func)
-    def wrapper(self, request, *args, **kwargs):
-        name = request.FILES[file].name
-        if re.match(REGEX_NAME, name) is None:
-            return Response(data={"message": "Invalid Name"}, status=status.HTTP_400_BAD_REQUEST)
-        result = func(self, request, *args, **kwargs)
-        return result
-    return wrapper
-
-
 def check_id(func):
 
     @functools.wraps(func)
     def wrapper(self, request, *args, **kwargs):
-        file_id = request.data["file_id"]
+        id = request.data["id"]
         profile = Profile.objects.get(user=request.user)
         filesystem = profile.filesystem
-        if file_id not in filesystem:
+        if id not in filesystem:
             return Response(data={"message": "Invalid file id"}, status=status.HTTP_400_BAD_REQUEST)
-        result = func(*args, **kwargs)
+        result = func(self, request, *args, **kwargs)
         return result
     return wrapper
 
@@ -68,15 +56,72 @@ def check_already_present(func):
     return wrapper
 
 
-def check_type(type_required):
+def check_parent_is_folder(func):
+
+    @functools.wraps(func)
+    def wrapper(self, request, *args, **kwargs):
+        profile = Profile.objects.get(user=request.user)
+        filesystem = profile.filesystem
+        parent = request.data[PARENT]
+        if filesystem[parent][TYPE] != FOLDER:
+            return Response(data={"message": "parent is not a folder"}, status=status.HTTP_400_BAD_REQUEST)
+        result = func(self, request, *args, **kwargs)
+        return result
+    return wrapper
+
+
+def check_file_name_from_request_file(func):
+    @functools.wraps(func)
+    def wrapper(self, request, *args, **kwargs):
+        name = request.FILES['file'].name
+        if re.match(REGEX_NAME, name) is None:
+            return Response(data={"message": "Invalid Name"}, status=status.HTTP_400_BAD_REQUEST)
+        result = func(self, request, *args, **kwargs)
+        return result
+    return wrapper
+
+
+def check_regex_file_name_from_request_body(func):
+    @functools.wraps(func)
+    def wrapper(self, request, *args, **kwargs):
+        new_name = request.data["name"]
+        if re.match(REGEX_NAME, new_name) is None:
+            return Response(data={"message": "Invalid Name"}, status=status.HTTP_400_BAD_REQUEST)
+        result = func(self, request, *args, **kwargs)
+        return result
+    return wrapper
+
+
+def check_equality_old_new_name(func):
+    @functools.wraps(func)
+    def wrapper(self, request, *args, **kwargs):
+        new_name = request.data["name"]
+        id = request.data["id"]
+
+        profile = Profile.objects.get(user=request.user)
+        filesystem = profile.filesystem
+        parent = filesystem[id][PARENT]
+        children = filesystem[parent][CHILDREN]
+
+        old_name = children[id][NAME]
+
+        if(new_name == old_name):
+            return Response(data={"message": "new name equals old name"}, status=status.HTTP_400_BAD_REQUEST)
+
+        result = func(self, request, *args, **kwargs)
+        return result
+    return wrapper
+
+
+def check_type_id(type_required):
     def decorator_check_type(func):
         @functools.wraps(func)
         def wrapper(self, request, *args, **kwargs):
-            parent = request.data[PARENT]
             profile = Profile.objects.get(user=request.user)
             filesystem = profile.filesystem
-            if filesystem[parent][TYPE] != type_required:
-                return Response(data={"message": "Parent is not a folder"}, status=status.HTTP_400_BAD_REQUEST)
+            id = request.data["id"]
+            if filesystem[id][TYPE] != type_required:
+                return Response(data={"message": "id is not of a {type_required}"}, status=status.HTTP_400_BAD_REQUEST)
             result = func(self, request, *args, **kwargs)
             # Do after
             return result
@@ -94,3 +139,34 @@ def check_request_attr(REQUIRED_PARAMS):
             return result
         return wrapper
     return decorator_check_type
+
+
+def check_already_present(to_check, type):
+    def decorator_func(func):
+        @functools.wraps(func)
+        def wrapper(self, request, *args, **kwargs):
+            profile = Profile.objects.get(user=request.user)
+            filesystem = profile.filesystem
+
+            if PARENT in request.data:
+                parent = request.data[PARENT]
+            else:
+                id = request.data["id"]
+                parent = filesystem[id][PARENT]
+
+            children = filesystem[parent][CHILDREN]
+
+            if(to_check == "req_data_name"):
+                name = request.data["name"]
+            elif to_check == "req_file_name":
+                name = request.FILES['file'].name
+
+            for id in children:
+                already_present_name = children[id][NAME]
+                already_present_type = children[id][TYPE]
+                if(name == already_present_name and already_present_type == type):
+                    return Response(data={"message": "Such a file/folder is already present"}, status=status.HTTP_400_BAD_REQUEST)
+            result = func(self, request, *args, **kwargs)
+            return result
+        return wrapper
+    return decorator_func
