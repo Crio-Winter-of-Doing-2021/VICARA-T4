6418bc8ce909c06734227b57e09c3919153adb7b
Pratik <pchy.393@gmail.com>
2021-03-19 14:47:07 +0530

    nothing changed

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export let baseURL = "http://localhost:8000";
 
-let token = "Token f03d4d5ec608e6a86622f16f292c05f0ceebf85b";
+let token = "Token 3eaac4c78b7b6b43664b270d416e9e8f874539cb";
 
 export default axios.create({
   baseURL,
