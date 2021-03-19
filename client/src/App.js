d6d3cc019d22db85d961e6867fbfdc4fed72b089
aashutoshPanda <iamashutoshpanda@gmail.com>
2021-03-19 20:29:52 +0530

    router settings

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import Dashboard from './Components/Dashboard/dashboard'
-import { BrowserRouter,Route,Redirect,Switch} from "react-router-dom";
+import React from "react";
+import Dashboard from "./Components/Dashboard/dashboard";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div style={{padding:"0 !important"}} className="App">
-      <BrowserRouter>
+    <div style={{ padding: "0 !important" }} className="App">
+      <Dashboard />
+      {/* <BrowserRouter>
        <Switch>
           <Route path='/' exact component={() => <Redirect to='/drive/ROOT' />} />
           <Route path='/drive/:id' component={Dashboard} />
@@ -13,7 +14,7 @@ function App() {
             <div>404 not found</div>
           </Route>
         </Switch> 
-      </BrowserRouter>
+      </BrowserRouter> */}
     </div>
   );
 }
--- client/src/Components/Dashboard/dashboard.js
+++ client/src/Components/Dashboard/dashboard.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import Structure from '../Structure/structure'
-import {sideNav} from '../../constants'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+import Structure from "../Structure/structure";
+import { sideNav } from "../../constants";
 
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import Favourite from "../Favourite/";
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
+    display: "flex",
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
@@ -32,7 +34,7 @@ const useStyles = makeStyles((theme) => ({
     width: drawerWidth,
   },
   drawerContainer: {
-    overflow: 'auto',
+    overflow: "auto",
   },
   content: {
     flexGrow: 1,
@@ -43,9 +45,9 @@ const useStyles = makeStyles((theme) => ({
 export default function ClippedDrawer(props) {
   const classes = useStyles();
 
-  const handlePageChange=(e,data)=>{
-    console.log(data)
-  }
+  const handlePageChange = (e, data) => {
+    console.log(data);
+  };
 
   return (
     <div className={classes.root}>
@@ -55,7 +57,6 @@ export default function ClippedDrawer(props) {
           <Typography variant="h6" noWrap>
             Vicara-T4
           </Typography>
-          
         </Toolbar>
       </AppBar>
       <Drawer
@@ -69,7 +70,11 @@ export default function ClippedDrawer(props) {
         <div className={classes.drawerContainer}>
           <List>
             {sideNav.map((data, index) => (
-              <ListItem button onClick={(e)=>handlePageChange(e,data.name)} key={data.name}>
+              <ListItem
+                button
+                onClick={(e) => handlePageChange(e, data.name)}
+                key={data.name}
+              >
                 <ListItemIcon>{data.icon}</ListItemIcon>
                 <ListItemText primary={data.name} />
               </ListItem>
@@ -77,9 +82,11 @@ export default function ClippedDrawer(props) {
           </List>
           <Divider />
           <List>
-            {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            {["All mail", "Trash", "Spam"].map((text, index) => (
               <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
                 <ListItemText primary={text} />
               </ListItem>
             ))}
@@ -88,7 +95,20 @@ export default function ClippedDrawer(props) {
       </Drawer>
       <main className={classes.content}>
         <Toolbar />
-        <Structure {...props} />
+        <BrowserRouter>
+          <Switch>
+            <Route
+              path="/"
+              exact
+              component={() => <Redirect to="/drive/ROOT" />}
+            />
+            <Route path="/drive/:id" component={Structure} />
+            <Route path="/fav/" component={Favourite} />
+            <Route path="*">
+              <div>404 not found</div>
+            </Route>
+          </Switch>
+        </BrowserRouter>
       </main>
     </div>
   );
