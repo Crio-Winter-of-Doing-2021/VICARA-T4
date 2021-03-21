2a94510382323aefc52529dfc92ea57754f94138
Pratik <pchy.393@gmail.com>
2021-03-21 23:22:43 +0530

    protected route added

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,21 @@ import React from "react";
 import Dashboard from "./Components/Dashboard/dashboard";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import NormalLoader from './Components/Loaders/normalBackdrop'
+import Login from './Components/Forms/login'
+import SignUp from './Components/Forms/signup'
 
 function App(props) {
   return (
     <div style={{ padding: "0 !important" }} className="App">
-      <Dashboard/>
-      <NormalLoader/>
+      <Switch>
+        <Route path="/login" component={Login}/>
+        <Route path="/signup" component={SignUp}/>
+        <Dashboard/>
+        <Route path="*">
+          <div>404 not found</div>
+        </Route>
+        <NormalLoader/>
+      </Switch>
     </div>
   );
 }
---
+++ client/src/Utilities/protectedRoute.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState} from 'react';
+import { Route, Redirect } from 'react-router-dom'
+import { useSelector } from "react-redux";
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  let [isAuthenticated, setIsAuthenticated] = useState(null)  
+  useEffect(() => {
+    let token = window.localStorage.getItem('lol')
+    if(token===null) setIsAuthenticated(false);
+    else setIsAuthenticated(true)
+    // eslint-disable-next-line
+  }, [])
+
+  if(isAuthenticated === null){
+    return <></>
+  }
+
+  return (
+    <Route {...rest} render={props =>
+      !isAuthenticated ? (
+        <Redirect to='/login'/>
+      ) : (
+        <Component {...props} />
+      )
+    }
+    />
+  );
+};
+
+export default PrivateRoute;
