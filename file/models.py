fa4bf3a00c43492947039b9847d7ea52b3532678
aashutoshPanda <iamashutoshpanda@gmail.com>
2021-03-26 18:57:11 +0530

    made models

--- file/models.py
+++ file/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from folder.models import Folder
+class File(models.Model):
+    pass
 
-# Create your models here.
+class File(models.Model):
+    file = models.FileField(blank=False, null=False)
+    parent = models.ManyToManyField(Folder, on_delete=models.CASCADE,related_name="children")
+    name = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    shared_among = models.ManyToManyField(User, on_delete=models.DO_NOTHING,related_name="shared_folders")
+    privacy = models.BooleanField(default=False)
+    trash = models.BooleanField(default=False)
--- folder/models.py
+++ folder/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Folder(models.Model):
+    pass
+
+class Folder(models.Model):
+    parent = models.ManyToManyField(Folder, on_delete=models.CASCADE,related_name="children")
+    name = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    shared_among = models.ManyToManyField(User, on_delete=models.DO_NOTHING,related_name="shared_folders")
+    privacy = models.BooleanField(default=False)
+    trash = models.BooleanField(default=False)
