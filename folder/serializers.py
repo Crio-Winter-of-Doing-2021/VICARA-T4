ac8ae8c8a21563fd35db8bec78d93ac2541cdb5e
aashutoshPanda <iamashutoshpanda@gmail.com>
2021-03-29 14:18:18 +0530

    added children to folders in response

--- folder/serializers.py
+++ folder/serializers.py
@@ -1,7 +1,11 @@
+from itertools import chain
+
+# django
 from rest_framework import serializers
 from .models import Folder
 from django.contrib.humanize.templatetags import humanize
 from django.contrib.auth.models import User
+from file.serializers import FileSerializer
 
 
 class FolderSerializer(serializers.ModelSerializer):
@@ -9,6 +13,7 @@ class FolderSerializer(serializers.ModelSerializer):
     created_at = serializers.SerializerMethodField()
     last_modified = serializers.SerializerMethodField()
     shared_among = serializers.SerializerMethodField()
+    children = serializers.SerializerMethodField()
 
     class Meta:
         model = Folder
@@ -20,6 +25,22 @@ class FolderSerializer(serializers.ModelSerializer):
 
     def get_last_modified(self, obj):
         return humanize.naturaltime(obj.last_modified)
+
+    def get_children(self, obj):
+        # folders
+        folders = obj.children_folder
+        folders = FolderSerializer(folders, many=True).data
+        for folder in folders:
+            folder["type"] = "folder"
+        # files
+        files = obj.children_file
+        files = FileSerializer(files, many=True).data
+        for file in files:
+            file["type"] = "file"
+
+        # combined
+        result_list = list(chain(folders, files))
+        return result_list
 
     def get_shared_among(self, obj):
         shared_among = []
