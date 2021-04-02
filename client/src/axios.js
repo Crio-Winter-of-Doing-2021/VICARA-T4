e45c881ad3fa238e822ba1e91bfe3cc34210c0d7
aashutoshPanda <iamashutoshpanda@gmail.com>
2021-04-03 02:04:42 +0530

    fixed multiple folders bug

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export let baseURL = "https://vicara-drf-backend.herokuapp.com";
-// export let baseURL = "http://localhost:8000";
+// export let baseURL = "https://vicara-drf-backend.herokuapp.com";
+export let baseURL = "http://localhost:8000";
 export let frontURL = "http://localhost:3000";
 export const token = window.localStorage.getItem("session");
 // console.log("token ", token);
