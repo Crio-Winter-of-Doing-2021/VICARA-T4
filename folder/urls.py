7a824912d833f8598d247f55d8c4f3a0fa281f30
aashutoshPanda <iamashutoshpanda@gmail.com>
2021-04-03 01:11:02 +0530

    removed rerdundant route

--- folder/urls.py
+++ folder/urls.py
@@ -1,9 +1,8 @@
-from .views import Filesystem, ShareFolder, UploadFolder, DownloadFolder
+from .views import Filesystem, UploadFolder, DownloadFolder
 from django.urls import path
 
 urlpatterns = [
     path('', Filesystem.as_view(), name="filesystem"),
-    path('share/', ShareFolder.as_view(), name="share-folder"),
     path('upload-folder/', UploadFolder.as_view(), name="upload-folder"),
 
     path('download/',  DownloadFolder.as_view(), name="download-folder"),
