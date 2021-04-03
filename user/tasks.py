b8dfe008b8262533a9d5001e22035c2a7ac5a25c
aashutoshPanda <iamashutoshpanda@gmail.com>
2021-04-03 12:29:33 +0530

    sending mail on share

--- user/tasks.py
+++ user/tasks.py
@@ -18,3 +18,13 @@ def send_mail(type, user_list, title_kwargs, body_kwargs):
                        }
         body = MAIL_DEFAULTS[type]["body"].format(**body_kwargs)
         sendMail(title, body, [user["email"]])
+
+
+def sync_send_mail(type, user_list, title_kwargs, body_kwargs):
+    for user in user_list:
+        title = MAIL_DEFAULTS[type]["title"].format(**title_kwargs)
+        body_kwargs = {**body_kwargs,
+                       "recepient_name": user["first_name"]
+                       }
+        body = MAIL_DEFAULTS[type]["body"].format(**body_kwargs)
+        sendMail(title, body, [user["email"]])
