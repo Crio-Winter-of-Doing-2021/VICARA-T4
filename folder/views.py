ba265b01a9ee2a2e3b5c6fac0f53e05e79ba5bd9
aashutoshPanda <iamashutoshpanda@gmail.com>
2021-04-08 21:25:50 +0530

    fixed folder delete response format

--- folder/views.py
+++ folder/views.py
@@ -152,7 +152,7 @@ class Filesystem(APIView):
         recursive_delete(folder, profile)
         profile.save()
         storage_data = ProfileSerializer(profile).data["storage_data"]
-        return Response(data={"id": id, **storage_data}, status=status.HTTP_200_OK)
+        return Response(data={"id": id, "storage_data": storage_data}, status=status.HTTP_200_OK)
 
 
 class UploadFolder(APIView):
